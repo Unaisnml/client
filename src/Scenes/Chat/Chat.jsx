863f148ceec6b08ca94fd3aa4d16998f42042f7c
unaisnml <unaisnml@gmail.com>
2023-02-03 17:49:10 +0530

    socket second

--- src/Scenes/Chat/Chat.jsx
+++ src/Scenes/Chat/Chat.jsx
@@ -28,7 +28,7 @@ const Chat = () => {
   }, [sendMessage]);
 
   useEffect(() => {
-    socket.current = io(`http://socket.cathchat.online`);
+    socket.current = io(`https://socket.cathchat.online`);
     socket.current.emit("new-user-add", user._id);
     socket.current.on("get-users", (users) => {
       setOnlineUsers(users);
